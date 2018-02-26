7c5999324fb6662df97d9a777ba5b8543fbf3eeb
Daniel <xdgaxd@gmail.com>
2018-02-26 11:02:29 +0100

    Finalizados las relaciones y la asignacion de un propietario

--- common/models/lista-familiar.js
+++ common/models/lista-familiar.js
@@ -1,5 +1,14 @@
 'use strict';
 
-module.exports = function(Listafamiliar) {
+module.exports = function (Listafamiliar) {
+
+    Listafamiliar.beforeRemote('create', function (context, Listafamiliar, next) {
+        //  console.log(context);  --> Contiene todo el objeto entero
+        // console.log(context.args.data.owner); --> Recoge el campo owner del POST /ListaFamiliars
+        // console.log(context.req.accessToken.userId); --> Recoge el id del usuario que está Autenticado
+        context.args.data.owner = context.req.accessToken.userId;
+        //Al crear una lista se le asigna el id del usuario que la ha creado.
+        next();
+    });
 
 };
